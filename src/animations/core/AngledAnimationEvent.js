25c8f6dc126c18eafd5369eecb15db85680a1702
XtremePlayzCODE <cmorgan513creeperz@gmail.com>
2017-04-29 11:23:01 -0500

    Whoopsie-doodle

--- src/animations/core/AngledAnimationEvent.js
+++ src/animations/core/AngledAnimationEvent.js
@@ -1,5 +1,5 @@
 // This is an interface and shouldn't be used, and isn't used in Angular bundles, but who cares?
-export default AngledAnimationEvent = {
+export default const AngledAnimationEvent = {
   fromState: "",
   toState: "",
   totalTime: 0,
