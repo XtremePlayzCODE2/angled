83a6fd7432a062a0786c2080af39532976c1aa08
XtremePlayzCODE <cmorgan513creeperz@gmail.com>
2017-04-29 10:46:28 -0500

    ES6 module definition for Angled
    * Note: This should be replace with real built code afterwards.

---
+++ dist/Angled.es6.js
@@ -0,0 +1,17 @@
+((factory, global) => {
+  // We're in a Web environment.
+  if (global instanceof window) {
+    let AngledWindowEnvironment = factory(global, 0);
+    AngledWindowEnvironment.HandleInternalDefinition();
+  } else if (typeof module !== `undefined` && typeof module.exports !== `undefined`) {
+    let AngledNodeEnvironment = factory(global, 1);
+    AngledNodeEnvironment.HandleInternalDefinition();
+  } else if (typeof exports !== `undefined`) {
+    let AngledCommonEnvironment = factory(global, 2);
+    AngledCommonEnvironment.HandleInternalDefinition();
+  } else {
+    throw new Error("[Angled] Unsupported distribution platform!");
+  }
+})((scope, platform) => {
+  
+}, this instanceof window ? window : this);
