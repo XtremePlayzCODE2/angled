bd94595b34ca5a36cc2a26f5fdcaacd2edaf8d98
XtremePlayzCODE <cmorgan513creeperz@gmail.com>
2017-04-29 11:20:10 -0500

    Add Angled animation events (Deprecated, read in-line comment)

---
+++ src/animations/core/AngledAnimationEvent.js
@@ -0,0 +1,9 @@
+// This is an interface and shouldn't be used, and isn't used in Angular bundles, but who cares?
+export default AngledAnimationEvent = {
+  fromState: "",
+  toState: "",
+  totalTime: 0,
+  phaseName: "",
+  element: {},
+  triggerName: ""
+}
