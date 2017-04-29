056045f99da5423b8a47747b9033a4d4d4ef95a0
XtremePlayzCODE <cmorgan513creeperz@gmail.com>
2017-04-29 15:23:51 -0500

    Whoops

--- dist/Angled.es5.js
+++ dist/Angled.es5.js
@@ -1,6 +1,6 @@
 (function (factory, global) {
   // We're in a Web environment.
-  if (global instanceof window) {
+  if (global === window) {
     var AngledWindowEnvironment = factory(global, 0);
     AngledWindowEnvironment.HandleInternalDefinition();
   } else if (typeof module !== `undefined` && typeof module.exports !== `undefined`) {
